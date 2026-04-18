58d99d4fb2d0b2d32f7cc997a78de1e822e19bb0
Ümit AYDIN <umitaydin@Umit-MacBook-Pro.local>
2026-04-18 23:28:34 +0300

    Revert "chore: add clean minaplay app"
    This reverts commit 7cfea2e8e1cb90b11f7874ed8b42fbd5b33770f2.

--- MinaPlay/server.ts
+++
@@ -1,8 +0,0 @@
-import { env } from './src/config/env';
-import { createApp } from './src/app';
-
-const app = createApp(env);
-
-app.listen(env.PORT, () => {
-  console.log(`Server running on port ${env.PORT}`);
-});
